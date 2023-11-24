c96cc1651150e1f7e996e1e87dbc5cbf35b30b3b
C0L2 <vacara7samue7@gmail.com>
2023-11-24 19:17:31 +0200

    deploy

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { configureStore } from '@reduxjs/toolkit';
 import { AddQuestion, FormData, Question, Survey, User } from './types';
 
-const URL = 'http://localhost:9500/';
+const URL = import.meta.env.VITE_API_URL;
 
 const baseQuery = fetchBaseQuery({
     baseUrl: URL,
